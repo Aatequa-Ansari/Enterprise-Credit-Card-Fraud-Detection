5ea6e28b973e11593ab81043b3cae450d494b15e
Aatequa Ansari <aatequaansari12@gmail.com>
2026-07-06 21:41:33 +0530

    Add Streamlit app

---
+++ app.py
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+import streamlit as st
+
+st.set_page_config(
+    page_title="Enterprise Credit Card Fraud Detection",
+    page_icon="💳",
+    layout="wide"
+)
+
+=======
+import streamlit as st
+
+st.set_page_config(
+    page_title="Enterprise Credit Card Fraud Detection",
+    page_icon="💳",
+    layout="wide"
+)
+
+>>>>>>> c1d02e34ece27a3c9b04165ab2f22b29b07b2f86
+st.switch_page("pages/01_📊_Dashboard.py")
